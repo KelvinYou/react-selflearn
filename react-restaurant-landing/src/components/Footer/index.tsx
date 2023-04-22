4c226becd9e2bcaa65ca56f5ea335be07e38c930
KelvinYou <ykekelvin0220@gmail.com>
2023-04-22 16:12:05 +0800

    remove redundant code

--- react-restaurant-landing/src/components/Footer/index.tsx
+++ react-restaurant-landing/src/components/Footer/index.tsx
@@ -42,25 +42,6 @@ const Footer: FC = () => {
               </div>
           </div>
       </div>
-      <div className="container">
-          <div className="copyright">
-              <div className="row">
-                  <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                      &copy; <a className="border-bottom" href="#">Your Site Name</a>, All Right Reserved. 
-        
-        Designed By <a className="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
-                  </div>
-                  <div className="col-md-6 text-center text-md-end">
-                      <div className="footer-menu">
-                          <a href="">Home</a>
-                          <a href="">Cookies</a>
-                          <a href="">Help</a>
-                          <a href="">FQAs</a>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
   </div>
   )
 }
