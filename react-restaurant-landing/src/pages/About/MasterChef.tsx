020d4887ce909dffd7428faf1ae7970ee2ff011c
Kelvin You <ykekelvin0220@gmail.com>
2023-04-25 08:55:18 +0800

    update detail

--- react-restaurant-landing/src/pages/About/MasterChef.tsx
+++ react-restaurant-landing/src/pages/About/MasterChef.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Container } from 'react-bootstrap';
+import Button from "@mui/material/Button";
 
 import FadeInSection from '../../components/FadeInSection';
 
@@ -8,79 +9,73 @@ import Team1 from "./../../assets/about/team-1.jpg";
 import Team2 from "./../../assets/about/team-2.jpg";
 import Team3 from "./../../assets/about/team-3.jpg";
 import Team4 from "./../../assets/about/team-4.jpg";
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 
+
+interface ChecItemInterface {
+  fullname: string,
+  role: string,
+  profilePicture: string
+}
+
+const chefItems: ChecItemInterface[] = [
+  {
+    fullname: "Full Name",
+    role: "Designation",
+    profilePicture: Team1
+  },
+  {
+    fullname: "Full Name",
+    role: "Designation",
+    profilePicture: Team2
+  },
+  {
+    fullname: "Full Name",
+    role: "Designation",
+    profilePicture: Team3
+  },
+  {
+    fullname: "Full Name",
+    role: "Designation",
+    profilePicture: Team4
+  },
+]
 
 const MasterChef = () => {
   return (
-    <Container fluid className="container-xxl pt-5 pb-3">
-      <FadeInSection>
+    <FadeInSection>
+      <Container fluid className="container-xxl pt-5 pb-3">
+      
         <div className="container">
           <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
-            <h5 className="section-title ff-secondary text-center text-primary fw-normal">Team Members</h5>
+            <h5 className="section-title secondary-title text-center fw-normal">Team Members</h5>
             <h1 className="mb-5">Our Master Chefs</h1>
           </div>
           <div className="row g-4">
-            <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-              <div className="team-item text-center rounded overflow-hidden">
-                <div className="rounded-circle overflow-hidden m-4">
-                  <img className="img-fluid" src={ Team1 } alt=""/>
-                </div>
-                <h5 className="mb-0">Full Name</h5>
-                <small>Designation</small>
-                <div className="d-flex justify-content-center mt-3">
-                  <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-facebook-f"></i></a>
-                  <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-twitter"></i></a>
-                  <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-instagram"></i></a>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-              <div className="team-item text-center rounded overflow-hidden">
-                <div className="rounded-circle overflow-hidden m-4">
-                  <img className="img-fluid" src={ Team2 } alt=""/>
-                </div>
-                <h5 className="mb-0">Full Name</h5>
-                <small>Designation</small>
-                <div className="d-flex justify-content-center mt-3">
-                  <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-facebook-f"></i></a>
-                  <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-twitter"></i></a>
-                  <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-instagram"></i></a>
+            {chefItems.map((chefItem, index) => (
+              <div 
+                key={ index + chefItem.fullname }
+                className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div className="team-item text-center rounded overflow-hidden">
+                  <div className="rounded-circle overflow-hidden m-4">
+                    <img className="img-fluid" src={ chefItem.profilePicture } alt=""/>
+                  </div>
+                  <h5 className="mb-0">{ chefItem.fullname }</h5>
+                  <small>{ chefItem.role }</small>
+                  <div className="d-flex justify-content-center mt-3">
+                    <Button className="btn"><FaFacebook /></Button>
+                    <Button className="btn" ><FaTwitter /></Button>
+                    <Button className="btn"><FaInstagram /></Button>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-              <div className="team-item text-center rounded overflow-hidden">
-                <div className="rounded-circle overflow-hidden m-4">
-                  <img className="img-fluid" src={ Team3 } alt=""/>
-                </div>
-                <h5 className="mb-0">Full Name</h5>
-                <small>Designation</small>
-                <div className="d-flex justify-content-center mt-3">
-                  <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-facebook-f"></i></a>
-                  <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-twitter"></i></a>
-                  <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-instagram"></i></a>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
-              <div className="team-item text-center rounded overflow-hidden">
-                <div className="rounded-circle overflow-hidden m-4">
-                  <img className="img-fluid" src={ Team4 } alt=""/>
-                </div>
-                <h5 className="mb-0">Full Name</h5>
-                <small>Designation</small>
-                <div className="d-flex justify-content-center mt-3">
-                  <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-facebook-f"></i></a>
-                  <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-twitter"></i></a>
-                  <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-instagram"></i></a>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
-      </FadeInSection>
       
-    </Container>
+      </Container>
+    </FadeInSection>
+
   )
 }
 
