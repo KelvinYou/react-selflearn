c95fc772ba4e729122506b596cc184b1389a567d
KelvinYou <ykekelvin0220@gmail.com>
2023-04-29 17:07:10 +0800

    update

--- react-restaurant-landing/src/pages/Menu/MenuContent.tsx
+++ react-restaurant-landing/src/pages/Menu/MenuContent.tsx
@@ -76,7 +76,7 @@ const MenuContent = () => {
     <FadeInSection>
       <Container> 
         <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
-          <h5 className="section-title ff-secondary text-center text-primary fw-normal">Food Menu</h5>
+          <h5 className="section-title secondary-title text-center fw-normal">Food Menu</h5>
           <h1 className="mb-5">Most Popular Items</h1>
         </div>
 
@@ -84,7 +84,7 @@ const MenuContent = () => {
           <ul className="nav nav-pills d-inline-flex justify-content-center border-bottom mb-5">
             <li className="nav-item">
               <a className="d-flex align-items-center text-start mx-3 ms-0 pb-3 active" data-bs-toggle="pill" href="#tab-1">
-                <i className="fa fa-coffee fa-2x text-primary"></i>
+                <i className="fa fa-coffee fa-2x"></i>
                 <div className="ps-3">
                   <small className="text-body">Popular</small>
                   <h6 className="mt-n1 mb-0">Breakfast</h6>
@@ -93,7 +93,7 @@ const MenuContent = () => {
             </li>
             <li className="nav-item">
               <a className="d-flex align-items-center text-start mx-3 pb-3" data-bs-toggle="pill" href="#tab-2">
-                <i className="fa fa-hamburger fa-2x text-primary"></i>
+                <i className="fa fa-hamburger fa-2x"></i>
                 <div className="ps-3">
                   <small className="text-body">Special</small>
                   <h6 className="mt-n1 mb-0">Launch</h6>
@@ -102,7 +102,7 @@ const MenuContent = () => {
             </li>
             <li className="nav-item">
               <a className="d-flex align-items-center text-start mx-3 me-0 pb-3" data-bs-toggle="pill" href="#tab-3">
-                <i className="fa fa-utensils fa-2x text-primary"></i>
+                <i className="fa fa-utensils fa-2x"></i>
                 <div className="ps-3">
                   <small className="text-body">Lovely</small>
                   <h6 className="mt-n1 mb-0">Dinner</h6>
@@ -120,7 +120,7 @@ const MenuContent = () => {
                     <div className="w-100 d-flex flex-column text-start ps-4">
                       <h5 className="d-flex justify-content-between border-bottom pb-2">
                         <span>{ menuItem.name }</span>
-                        <span className="text-primary">{ currency + " " } { menuItem.price }</span>
+                        <span className="">{ currency + " " } { menuItem.price }</span>
                       </h5>
                       <small className="fst-italic">{ menuItem.description }</small>
                     </div>
